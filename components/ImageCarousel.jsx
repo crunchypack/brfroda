9785f9cd3c2bbcda2a21f1f3bc0610bc4cc10dd8
Simon <simon.wolf.lobo@gmail.com>
2024-03-14 08:18:49 +0100

    crying

--- components/ImageCarousel.jsx
+++ components/ImageCarousel.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+"use client"
+import { useEffect, useRef, useState } from "react"
+import Image from "next/image"
+import { Carousel } from "react-responsive-carousel"
+import "react-responsive-carousel/lib/styles/carousel.min.css"; 
 
 const ImageCarousel = () => {
   return (
-    <div>ImageCarousel</div>
+    
+      <Carousel showThumbs={false}>
+        <div>
+          <image src='https://placehold.co/600x400/orange/white' /> 
+          <p className="legend">Image </p>
+        </div>
+        <div>
+          <image src='https://placehold.co/600x400/black/white' />
+          <p className="legend">Image </p>
+        </div>
+      </Carousel>
+   
+
   )
 }
 
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images:{
+        domains:['https://placehold.co/']
+    }
+};
 
 export default nextConfig;
