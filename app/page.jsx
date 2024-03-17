128b67e08398581a63a087123742657d21d8a299
Simon <simon.wolf.lobo@gmail.com>
2024-03-17 08:49:54 +0100

    image carousel

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,14 @@
 import ImageCarousel from "@/components/ImageCarousel";
 import Image from "next/image";
 
+const OPTIONS = { loop: true }
+const SLIDE_COUNT = 5
+const SLIDES = Array.from(Array(SLIDE_COUNT).keys())
+
 export default function Home() {
   return (
-    <main>Hello
-      <ImageCarousel/>
-    </main>
+    <div>
+      <ImageCarousel slides={SLIDES} options={OPTIONS} />
+    </div>
   );
 }
--- components/ImageCarousel.jsx
+++ components/ImageCarousel.jsx
@@ -1,24 +1,57 @@
 "use client"
 import { useEffect, useRef, useState } from "react"
 import Image from "next/image"
-import { Carousel } from "react-responsive-carousel"
-import "react-responsive-carousel/lib/styles/carousel.min.css"; 
+import { DotButton,useDotButton } from "./ImageDotButton"
+import useEmblaCarousel from 'embla-carousel-react'
+import {PrevButton,NextButton,
+  usePrevNextButtons} from "./ImageArrow"
 
-const ImageCarousel = () => {
+const ImageCarousel = ({slides, options}) => {
+  
+ 
+  const [emblaRef, emblaApi] = useEmblaCarousel(options)
+
+  const { selectedIndex, scrollSnaps, onDotButtonClick } =
+    useDotButton(emblaApi)
+
+  const {
+    prevBtnDisabled,
+    nextBtnDisabled,
+    onPrevButtonClick,
+    onNextButtonClick
+  } = usePrevNextButtons(emblaApi)
+
   return (
-    
-      <Carousel showThumbs={false}>
-        <div>
-          <image src='https://placehold.co/600x400/orange/white' /> 
-          <p className="legend">Image </p>
+    <section className="embla">
+      <div className="embla__viewport" ref={emblaRef}>
+        <div className="embla__container">
+          {slides.map((index) => (
+            <div className="embla__slide" key={index}>
+              <div className="embla__slide__number">{index + 1}</div>
+            </div>
+          ))}
         </div>
-        <div>
-          <image src='https://placehold.co/600x400/black/white' />
-          <p className="legend">Image </p>
-        </div>
-      </Carousel>
-   
+      </div>
 
+      <div className="embla__controls">
+        <div className="embla__buttons">
+          <PrevButton onClick={onPrevButtonClick} disabled={prevBtnDisabled} />
+          <NextButton onClick={onNextButtonClick} disabled={nextBtnDisabled} />
+        </div>
+
+        <div className="embla__dots">
+          {scrollSnaps.map((_, index) => (
+            <DotButton
+              key={index}
+              onClick={() => onDotButtonClick(index)}
+              className={'embla__dot'.concat(
+                index === selectedIndex ? ' embla__dot--selected' : ''
+              )}
+            />
+          ))}
+        </div>
+      </div>
+    </section>
   )
 }
 
